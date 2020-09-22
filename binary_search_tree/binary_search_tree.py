87619618dda19e7f7f48586b17488dd630ec9a20
Elijah Atkins <elijahorama@gmail.com>
2020-09-21 21:22:56 -0600

    day3 first commit

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -15,29 +15,94 @@ class BSTNode:
         self.left = None
         self.right = None
 
+    def get_next(self):
+        if self.right:
+            return self.right
+        return None
+
     # Insert the given value into the tree
+        #the Left subtree of a node contains only nodes with values lesser than the node's value
+        #the right subtree of a node contains only nodes with a value greater than the node's value
+        #the left and right subtree each must also be a binary search tree
     def insert(self, value):
-        pass
+        if value <= self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+        elif value > self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+    # recursively check the tree values
+        if target is self.value:
+            return True
+        if target <= self.value:
+            if self.left is None:
+                return False
+            return self.left.contains(target)
+        if target > self.value:
+            if self.right is None:
+                return False
+            return self.right.contains(target)
+
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        #check if head exist if not assume empty return None
+        if not self.right:
+            return None
+        #set initial max value as item in head
+        max_value = self.right.value
+        #start at head
+        current_node = self.right
+        while current_node:
+            if current_node.value > max_value:
+                #check value in current node if greater than 
+                #max_value set current value as new max
+                max_value = current_node.value
+            #cycle to next node
+            current_node = current_node.get_next()
+        #return higest value
+        return max_value
+
+
+
+
+    #     #recursive 
+    # def get_max(self):
+
+    #     if self.right is None:
+    #         return self.value
+    #     return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        #call the function passing in the current nodes value
+        #if there is a node to the left call the function on left value
+        if self.left:
+            self.left.for_each(fn)
+        #if there is a node on the right call the function on the right value
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self):
-        pass
+        if self.left:
+            self.left.in_order_print()
+        print(self.value)
+        if self.right:
+            self.right.in_order_print()
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
@@ -65,21 +130,25 @@ This code is necessary for testing the `print` methods
 """
 bst = BSTNode(1)
 
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
 bst.insert(2)
-
+bst.insert(3)
+bst.insert(8)
+bst.insert(4)
+bst.insert(5)
+bst.insert(6)
+bst.insert(7)
 bst.bft_print()
 bst.dft_print()
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
+
+print(bst.get_max())
+
+
+
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
 print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+bst.in_order_print()
+#print("post order")
+#bst.post_order_dft()  
